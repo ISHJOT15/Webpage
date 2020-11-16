700c6104b3b755908cdf8e515e7bd4fad1b2ded4
ISHJOT <60621772+ISHJOT15@users.noreply.github.com>
2020-11-16 11:14:43 +0530

    Update resp.js

--- SWaG/resp.js
+++ SWaG/resp.js
@@ -1,16 +1,3 @@
-//var burger = document.querySelector(".burger")
-//var navbar=document.querySelector(".navbar")
-//var navlist=document.querySelector(".navlist")
-//var rightNav=document.querySelector(".rightNav")
-
-//burger.addEventListener("click", toggleHamburger)
-
-//function toggleHamburger(){
-    //rightNav.classlist.toggle("v-class-resp")
-    //navlist.classlist.toggle("v-class-resp")
-    //navbar.classlist.toggle("h-nav-resp")
-//}
-
 var burger = document.querySelector(".burger")
 var navbar = document.querySelector(".navbar")
 var navlist= document.querySelector(".navlist")
